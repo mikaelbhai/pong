13bb3d6cafe596f546d52817598ae5ce6141cfcd
MikaelBhai <126885689+mikaelbhai@users.noreply.github.com>
2023-04-30 12:48:56 +0500

    Update pong.js

--- pong.js
+++ pong.js
@@ -5,91 +5,82 @@ var ctx = canvas.getContext("2d");
 // Set initial ball position and velocity
 var ballX = canvas.width/2;
 var ballY = canvas.height/2;
-var ballSpeedX = 2;
-var ballSpeedY = 2;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
 
 // Set initial paddle positions
 var leftPaddleY = canvas.height/2 - 50;
 var rightPaddleY = canvas.height/2 - 50;
 
 // Set up keyboard controls for left paddle
-var leftPaddleMovingUp = false;
-var leftPaddleMovingDown = false;
-
+var upArrowPressed = false;
+var downArrowPressed = false;
 document.addEventListener("keydown", function(event) {
     if (event.keyCode == 38) {
-        leftPaddleMovingUp = true;
+        upArrowPressed = true;
     }
     if (event.keyCode == 40) {
-        leftPaddleMovingDown = true;
+        downArrowPressed = true;
     }
 });
-
 document.addEventListener("keyup", function(event) {
     if (event.keyCode == 38) {
-        leftPaddleMovingUp = false;
+        upArrowPressed = false;
     }
     if (event.keyCode == 40) {
-        leftPaddleMovingDown = false;
+        downArrowPressed = false;
     }
 });
 
-// Move the left paddle
-function moveLeftPaddle() {
-    if (leftPaddleMovingUp) {
-        leftPaddleY -= 10;
+// Set up keyboard controls for right paddle
+var wPressed = false;
+var sPressed = false;
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 87) {
+        wPressed = true;
     }
-    if (leftPaddleMovingDown) {
-        leftPaddleY += 10;
+    if (event.keyCode == 83) {
+        sPressed = true;
     }
-}
+});
+document.addEventListener("keyup", function(event) {
+    if (event.keyCode == 87) {
+        wPressed = false;
+    }
+    if (event.keyCode == 83) {
+        sPressed = false;
+    }
+});
 
 // Draw the game objects
 function draw() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw the ball and paddles
-    // ...
-
-    // Move the left paddle
-    moveLeftPaddle();
-
-    // ...
-}
-
-
-// Draw the game objects
-function draw() {
-    // Clear the canvas
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     // Draw the ball
     ctx.beginPath();
-    ctx.arc(ballX, ballY, 5, 0, Math.PI*2);
+    ctx.arc(ballX, ballY, 10, 0, Math.PI*2);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.closePath();
 
     // Draw the paddles
     ctx.fillStyle = "white";
-    ctx.fillRect(10, leftPaddleY, 5, 50);
-    ctx.fillRect(canvas.width-15, rightPaddleY, 5, 50);
+    ctx.fillRect(10, leftPaddleY, 10, 100);
+    ctx.fillRect(canvas.width-20, rightPaddleY, 10, 100);
 
     // Move the ball
     ballX += ballSpeedX;
     ballY += ballSpeedY;
 
     // Bounce the ball off the top and bottom walls
-    if (ballY < 5 || ballY > canvas.height-5) {
+    if (ballY < 10 || ballY > canvas.height-10) {
         ballSpeedY = -ballSpeedY;
     }
 
     // Check if the ball collides with the left paddle
-    if (ballX < 15 && ballY > leftPaddleY && ballY < leftPaddleY + 50) {
-        ballSpeedX = -ballSpeedX;
-    }
+    if (ballX < 30 && ballY > leftPaddleY &&
+
 
     // Check if the ball collides with the right paddle
     if (ballX > canvas.width-15 && ballY > rightPaddleY && ballY < rightPaddleY + 50) {
