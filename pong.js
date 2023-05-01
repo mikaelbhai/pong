fd5090dec18bb799c912c91af723bd5bec1ba74b
MikaelBhai <126885689+mikaelbhai@users.noreply.github.com>
2023-05-01 12:37:56 +0500

    Update pong.js

--- pong.js
+++ pong.js
@@ -219,6 +219,8 @@ if (leftPaddleDown && leftPaddleY < canvas.height - 100) {
     leftPaddleY += paddleSpeed;
 }
 }
+// add this line to call updateScore() at the end of draw()
+updateScore();
 }
 
 // Update the game every 10 milliseconds
